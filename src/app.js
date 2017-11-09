3315262e0336bd8d154956746b73d1397db903b9
Isaac Torres <isaac@crayon.com.mx>
2017-11-09 15:37:35 -0600

    Añadimos HTML Loader

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
+if ("production" !== process.env.NODE_ENV) {
+  var html = require("../public/index.html");
+}
+
 document.getElementById("app").innerHTML = "Este es nuestro proyecto";
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,29 @@ var path = require("path");
 
 module.exports = {
   entry: {
-    app: './src/app.js',
+    app: "./src/app.js"
   },
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'dist/bundle.js',
+    path: path.join(__dirname, "public"),
+    filename: "dist/bundle.js"
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    open: true,
+    contentBase: path.join(__dirname, "public"),
+    open: true
   },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
